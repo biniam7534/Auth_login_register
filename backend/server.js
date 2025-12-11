a6cd3738d2d92c88fd200a3e3cafcf351d4af418
Biniam <biniamabuu75@gmail.com>
2025-12-11 16:14:30 +0300

    initial

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 // import cors from 'cors';
-import connectDB from './config/db.js';
-
+import {connectDB} from './config/db.js';
+import authRoutes from './routes/auth.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req,res)=> {
-    res.send("biniam");
-});
-
+app.use("/api/users", authRoutes)
 connectDB();
 
 app.listen(PORT, () => {
